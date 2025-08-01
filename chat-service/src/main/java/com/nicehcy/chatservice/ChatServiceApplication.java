3da43e94621deb46f7f32959c9e2fef5a877185c
nicehcy <nicehcy2@naver.com>
2025-08-01 16:50:04 +0900

    디렉토리 이름 변경

--- chat-service/src/main/java/com/nicehcy/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/com/nicehcy/chatservice/ChatServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatserviceApplication {
+public class ChatServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatserviceApplication.class, args);
+		SpringApplication.run(ChatServiceApplication.class, args);
 	}
 
 }
