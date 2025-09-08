22e3a9fce87ee971aa02653693c540437584f721
nicehcy <nicehcy2@naver.com>
2025-09-08 18:31:02 +0900

    init

---
+++ chat-api-service/src/main/java/com/nicehcy2/chatapiservice/ChatApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nicehcy2.chatapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApiServiceApplication.class, args);
+	}
+
+}
---
+++ chat-api-service/src/test/java/com/nicehcy2/chatapiservice/ChatApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicehcy2.chatapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
