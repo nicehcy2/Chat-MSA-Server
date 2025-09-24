0ce0b1834a08bb31dae126c5241dabfbc29bcb5e
nicehcy <nicehcy2@naver.com>
2025-09-25 01:26:17 +0900

    컨피그 서버 로직 및 RefreshScope 추가

--- chat-service/src/main/java/com/nicehcy/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/com/nicehcy/chatservice/ChatServiceApplication.java
@@ -2,8 +2,10 @@ package com.nicehcy.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
