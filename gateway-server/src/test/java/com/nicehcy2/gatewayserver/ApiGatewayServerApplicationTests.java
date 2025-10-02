f40170f8c8338984e379fa2eed7d2ac42b6f19a9
nicehcy <nicehcy2@naver.com>
2025-10-02 19:31:26 +0900

    gateway-server init

---
+++ gateway-server/src/test/java/com/nicehcy2/gatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicehcy2.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
