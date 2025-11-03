7ac98440b314ae54dc7cc86bd0879212ed159ce4
nicehcy <nicehcy2@naver.com>
2025-11-03 22:46:02 +0900

    집킨 설정

--- gateway-server/src/main/java/com/nicehcy2/gatewayserver/ApiGatewayServerApplication.java
+++ gateway-server/src/main/java/com/nicehcy2/gatewayserver/ApiGatewayServerApplication.java
@@ -2,11 +2,14 @@ package com.nicehcy2.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
+
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
 
