79af8c4801c62abd16e8d1e7f45a7693a27c6f02
heo <nicehcy2@naver.com>
2026-02-22 17:07:54 +0900

    [FEAT] CONNECT 시 JWT 검증 및 Redis에 sessionId 등록

---
+++ chat-service/src/main/java/com/nicehcy/chatservice/common/JwtProvider.java
@@ -0,0 +1,56 @@
+package com.nicehcy.chatservice.common;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Slf4j
+@Component
+public class JwtProvider {
+
+    private final Key key;
+
+    public JwtProvider(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    /**
+     *
+     * @param token Jwt 토큰
+     * @return JWT를 검증하고 Payload를 꺼내서 Claims 객체로 반환
+     */
+    public Claims parseClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key) // 키가 맞지 않으면 예외 발생. 서명 검증에 사용할 비밀키 설정
+                .build()
+                .parseClaimsJws(token) // 토큰 파싱과 동시에 서명 검증 + 만료 검사 + 형식 검사
+                .getBody(); // Payload를 꺼냄
+    }
+
+    public boolean validate(String token) {
+        try {
+            parseClaims(token); // 파싱 + 검증
+            return true;
+        } catch (ExpiredJwtException e) {
+
+            log.info("JWT Token 만료!");
+            return false;
+        }
+        catch (JwtException | IllegalArgumentException e) {
+
+            log.info("JWT 토큰 검증 실패(만료가 원인은 아님)");
+            return false;
+        }
+    }
+}
+
--- chat-service/src/main/java/com/nicehcy/chatservice/config/socket/SocketSessionInterceptor.java
+++ chat-service/src/main/java/com/nicehcy/chatservice/config/socket/SocketSessionInterceptor.java
@@ -1,5 +1,7 @@
 package com.nicehcy.chatservice.config.socket;
 
+import com.nicehcy.chatservice.common.JwtProvider;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -7,9 +9,9 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -17,53 +19,44 @@ import org.springframework.stereotype.Component;
 public class SocketSessionInterceptor implements ChannelInterceptor {
 
     private final SocketConnectionTracker socketConnectionTracker;
+    private final JwtProvider jwtProvider;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
 
         // STOMP 헤더 정보를 래핑합니다.
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        accessor.setUser(auth);
-
-        // TODO: Authorization 가져오기
-        if (accessor.getCommand() == null || accessor.getNativeHeader("x-user-id") == null) {
-            return message;
-        }
-
-        Long userId = Long.parseLong(accessor.getNativeHeader("x-user-id").get(0));
 
         // STOMP 명령어에 따라 처리합니다.
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
 
-            // 로그인 없는 버전
-            socketConnectionTracker.setUserOnline(userId);
-            log.info("User {} is now ONLINE.", userId);
-
-            // TODO: 인증 추가
-            /*
-            if (accessor.getUser() != null) {
-                // REST API와 동일하게 FindLoginUser를 통해 사용자 ID를 확인합니다.
-                String email = accessor.getUser().getName();
-                // Long userId = FindLoginUser.toId(email);
-                // socketConnectionTracker.setUserOnline();
-                // log.info("User {} is now ONLINE.", userId);
+            String authHeader = accessor.getFirstNativeHeader("Authorization");
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                throw new IllegalStateException("Unauthorized");
             }
-             */
+
+            String token = authHeader.substring(7);
+            if (!jwtProvider.validate(token)) {
+                throw new IllegalStateException("Invalid token");
+            }
+
+            Claims claims = jwtProvider.parseClaims(token);
+            String userId = String.valueOf(claims.get("userId"));
+
+            // sessionAttributes에 userId 저장
+            accessor.getSessionAttributes().put("userId", userId);
+            socketConnectionTracker.setUserOnline(Long.parseLong(userId));
+
+            log.info("User {} is now ONLINE.", userId);
         } else if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
 
-            socketConnectionTracker.setUserOffline(userId);
-            log.info("User {} is now ONLINE.", userId);
+            Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
+            if (sessionAttributes != null && sessionAttributes.containsKey("userId")) {
+                Long userId = Long.parseLong((String) sessionAttributes.get("userId"));
+                socketConnectionTracker.setUserOffline(userId);
 
-            /*
-            if (accessor.getUser() != null) {
-                String email = accessor.getUser().getName();
-
-                // Long userId = FindLoginUser.toId(email);
-                // tracker.setUserOffline(userId);
-                // log.info("User {} is now OFFLINE.", userId);
+                log.info("User {} is now OFFLINE.", userId);
             }
-             */
         }
 
         return message;
