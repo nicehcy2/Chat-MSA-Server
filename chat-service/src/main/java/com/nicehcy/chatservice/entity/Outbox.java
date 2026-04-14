fcf04f7c8fd264347b1e30397a8201c015444897
heo <nicehcy2@naver.com>
2026-04-14 16:37:13 +0900

    [FEAT] Getter 추가

--- chat-service/src/main/java/com/nicehcy/chatservice/entity/Outbox.java
+++ chat-service/src/main/java/com/nicehcy/chatservice/entity/Outbox.java
@@ -5,10 +5,12 @@ import com.nicehcy.chatservice.entity.enums.OutboxStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Entity
 @Builder
 @NoArgsConstructor
