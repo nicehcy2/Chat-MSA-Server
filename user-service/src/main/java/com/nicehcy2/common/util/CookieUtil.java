bfe59d4f8bbc1976b8214e7d81c6725be94192ab
heo <nicehcy2@naver.com>
2026-08-20 16:37:28 +0900

    [REFACTOR] CookieUtil static 메서드를 일반 메서드로 변경
    static 메서드를 사용하면, 해당 메서드를 사용하고 테스트할 때 코드를 비교적 간편하게 작성할 수 있다는 이점이 있습니다. 그러나 외부에서 REFRESH_TTL 값을 받아야하는 지금 상황에서 static 메서드를 사용하려면 하드 코딩을 하는 방법 밖에 없습니다.
    
    설정을 외부에서 관리하는 것을 원하기 때문에 static 메서드를 Component 빈에서 관리하는 것으로 변경했습니다.

--- user-service/src/main/java/com/nicehcy2/common/util/CookieUtil.java
+++ user-service/src/main/java/com/nicehcy2/common/util/CookieUtil.java
@@ -2,30 +2,35 @@ package com.nicehcy2.common.util;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
-import static com.nicehcy2.common.util.JwtUtil.REFRESH_TTL;
-
+@Component
+@RequiredArgsConstructor
 public class CookieUtil {
 
-    public static void addAuthCookies(HttpServletResponse response,
-                                      String refreshToken,
-                                      String sessionId) {
+    private final JwtUtil jwtUtil;
+
+    public void addAuthCookies(HttpServletResponse response,
+                               String refreshToken,
+                               String sessionId) {
         response.addCookie(createHttpOnlyCookie(
-                "refreshToken", refreshToken, REFRESH_TTL
+                "refreshToken", refreshToken, jwtUtil.getREFRESH_TTL()
         ));
         response.addCookie(createHttpOnlyCookie(
-                "sessionId", sessionId, REFRESH_TTL
+                "sessionId", sessionId, jwtUtil.getREFRESH_TTL()
         ));
     }
 
-    private static Cookie createHttpOnlyCookie(String name, String value, Duration maxAge) {
+    private Cookie createHttpOnlyCookie(String name, String value, Duration maxAge) {
 
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         cookie.setMaxAge(Math.toIntExact(maxAge.getSeconds()));
+        // TODO: 2단계에서 Secure, SameSite 설정 추가
 
         return cookie;
     }
