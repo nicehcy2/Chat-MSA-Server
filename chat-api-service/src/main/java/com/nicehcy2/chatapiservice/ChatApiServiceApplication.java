bf2fd0ef2d7a22f97ed19f0abb3e4b9812e698f5
heo <nicehcy2@naver.com>
2026-09-02 20:10:48 +0900

    [FIX] chat-api-service에 JPA Auditing 활성화
    @EnableJpaAuditing import만 있고 어노테이션이 빠져 있어
    BaseEntity의 @CreatedDate/@LastModifiedDate가 동작하지 않았다.
    이 상태로 INSERT하면 created_at/updated_at/joined_at이 NULL로 저장된다.
    (현재는 chat-api-service에 쓰기 경로가 없어 드러나지 않았고,
    방 생성/입장 API 추가 전에 선행 수정)

--- chat-api-service/src/main/java/com/nicehcy2/chatapiservice/ChatApiServiceApplication.java
+++ chat-api-service/src/main/java/com/nicehcy2/chatapiservice/ChatApiServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class ChatApiServiceApplication {
 
 	public static void main(String[] args) {
